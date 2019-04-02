dd8df6e535cf4b27552806edc624b953950cc670
mimikun <karaage_nennen@icloud.com>
2019-04-02 20:14:13 +0900

    MAX_TOOT_CHARを定数にした

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -25,6 +25,8 @@ import Icon from 'mastodon/components/icon';
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
+const MAX_TOOT_CHAR = 5000
+
 const messages = defineMessages({
   placeholder: { id: 'compose_form.placeholder', defaultMessage: 'What is on your mind?' },
   spoiler_placeholder: { id: 'compose_form.spoiler_placeholder', defaultMessage: 'Write your warning here' },
@@ -104,7 +106,7 @@ class ComposeForm extends ImmutablePureComponent {
     const { is_submitting, is_changing_upload, is_uploading, anyMedia } = this.props;
     const fulltext = [this.props.spoiler_text, countableText(this.props.text)].join('');
 
-    if (is_submitting || is_uploading || is_changing_upload || length(fulltext) > 5000 || (fulltext.length !== 0 && fulltext.trim().length === 0 && !anyMedia)) {
+    if (is_submitting || is_uploading || is_changing_upload || length(fulltext) > MAX_TOOT_CHAR || (fulltext.length !== 0 && fulltext.trim().length === 0 && !anyMedia)) {
       return;
     }
 
@@ -196,7 +198,7 @@ class ComposeForm extends ImmutablePureComponent {
     const disabled = this.props.is_submitting;
     const preTagTemplate = tagTemplate && tagTemplate.length > 0 ? ' #' : '';
     const text     = [this.props.spoiler_text, countableText(this.props.text), preTagTemplate+tagTemplate].join('');
-    const disabledButton = disabled || this.props.is_uploading || this.props.is_changing_upload || length(text) > 5000 || (text.length !== 0 && text.trim().length === 0 && !anyMedia);
+    const disabledButton = disabled || this.props.is_uploading || this.props.is_changing_upload || length(text) > MAX_TOOT_CHAR || (text.length !== 0 && text.trim().length === 0 && !anyMedia);
     let publishText = '';
 
     if (this.props.privacy === 'private' || this.props.privacy === 'direct') {
@@ -259,7 +261,7 @@ class ComposeForm extends ImmutablePureComponent {
             <SensitiveButtonContainer />
             <SpoilerButtonContainer />
           </div>
-          <div className='character-counter__wrapper'><CharacterCounter max={5000} text={text} /></div>
+          <div className='character-counter__wrapper'><CharacterCounter max={MAX_TOOT_CHAR} text={text} /></div>
         </div>
 
         <div className='compose-form__publish'>
